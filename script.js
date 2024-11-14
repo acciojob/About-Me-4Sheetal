d13cd905db48f8302323af9e542c4b8e3bfd9298
4Sheetal <38429119+4Sheetal@users.noreply.github.com>
2024-11-14 10:38:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Your code here.
+document.addEventListener('DOMContentLoaded', () => {
+    const skillLevels = {
+        react: 95,
+        css: 95,
+        javascript: 95,
+        nodejs: 90,
+        mongodb: 90,
+        python: 99,
+        aws: 93,
+    };
+
+    Object.keys(skillLevels).forEach(skill => {
+        const bar = document.querySelector(`.progress.${skill}`);
+        bar.style.width = `${skillLevels[skill]}%`;
+    });
+});
